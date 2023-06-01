a2bc3167b17d41a56dc1e60898b212237672a314
Mardi123 <tejarahulmardi009@gmail.com>
2023-06-01 17:41:08 +0530

    Update views.py

--- shops/views.py
+++ shops/views.py
@@ -4,6 +4,7 @@ from .forms import ShopForm
 from django.views import View
 from django.db.models import F, FloatField
 from django.db.models.functions import Power, Sqrt
+from django.views.decorators.csrf import csrf_protect
 
 def shop_list(request):
     shops = Shop.objects.all()
@@ -14,6 +15,8 @@ def shop_detail(request, shop_id):
     return render(request, 'shop_detail.html', {'shop': shop})
 def shop_get(request):
     return render(request, 'base.html')
+
+@csrf_protect
 def shop_create(request):
     if request.method == 'POST':
         form = ShopForm(request.POST)
@@ -24,6 +27,7 @@ def shop_create(request):
         form = ShopForm()
     return render(request, 'shop_form.html', {'form': form})
 
+@csrf_protect
 def shop_update(request, shop_id):
     shop = get_object_or_404(Shop, pk=shop_id)
     if request.method == 'POST':
@@ -35,16 +39,18 @@ def shop_update(request, shop_id):
         form = ShopForm(instance=shop)
     return render(request, 'shop_form.html', {'form': form})
 
+@csrf_protect
 def shop_delete(request, shop_id):
     shop = get_object_or_404(Shop, pk=shop_id)
     if request.method == 'POST':
         shop.delete()
         return redirect('shop_list')
     return render(request, 'shop_confirm_delete.html', {'shop': shop})
+
 class ShopQueryView(View):
     def get(self, request):
         return render(request, 'shop_query.html')
-
+    @csrf_protect
     def post(self, request):
         latitude = float(request.POST.get('latitude'))
         longitude = float(request.POST.get('longitude'))
